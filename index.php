bc7288e801a727c1eae46b6f195acf07d8a48ba7
gabriell-silva <gabrielsilva.freires2@gmail.com>
2022-03-14 17:06:39 -0300

    hello world in php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Hello-World</title>
+</head>
+<body>
+    <div class="container">
+        <p class="p"><?php echo 'Hello World!';?></p>
+    </div>
+</body>
+</html>
